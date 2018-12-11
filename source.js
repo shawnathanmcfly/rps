914ed14bb185ecad62c9b8ef391b12eb71fef1a9
shawnathanmcfly <sachimasi9187@gmail.com>
2018-12-11 14:43:28 -0500

    database stuff

--- source.js
+++ source.js
@@ -1,7 +1,5 @@
 
-// GLOBALS
-
-var c = document.getElementById("gameWindow");
+var c = document.getElementById("game-window");
 var ctx = c.getContext("2d");
 var bg = document.getElementById("bg");
 var rock = document.getElementById("rock");
@@ -11,9 +9,7 @@ var rock1 = 0;
 var paper1 = 1;
 var cut1 = 2;
 
-var rock1 = 0;
-var paper1 = 1;
-var cut1 = 2;
+var myId;
 var playerScore = 0;
 var oppScore = 0;
 
@@ -25,19 +21,28 @@ function pick( choice ){
 
     switch( choice ){
         case rock1:
-        ctx.drawImage(rock, 800 / 2 - 100, 400 - 100, 100, 100);
+        ctx.drawImage(rock, c.width / 2 - 100, c.height - 100, c.width / 10, 100);
+        firebase.database().ref().child( myId ).update({
+            choice: "rock"
+        });
         break;
         case paper1:
-        ctx.drawImage(paper, 800 / 2 - 100, 400 - 100, 100, 100);
+        ctx.drawImage(paper, c.width / 2 - 100, c.height - 100, c.width / 10, 100);
+        firebase.database().ref().child( myId ).update({
+            choice: "paper"
+        });
         break;
         case cut1:
-        ctx.drawImage(cut, 800 / 2 - 100, 400 - 100, 100, 100);
+        ctx.drawImage(cut, c.width / 2 - 100, c.height - 100, c.width / 10, 100);
+        firebase.database().ref().child( myId ).update({
+            choice: "cut"
+        });
         break;
         default:
         break;
     }
 
-    opp = Math.floor(Math.random() * 3);
+    /*opp = Math.floor(Math.random() * 3);
     if( opp === rock1 ){
         ctx.drawImage(rock, 800 / 2 - 100, 5, 100, 100);
     }else if( opp === paper1 ){
@@ -65,7 +70,7 @@ function pick( choice ){
         ctx.font="20px Georgia";
         ctx.fillText("YOU LOSE!",10,50);
         oppScore++;
-    }
+    }*/
 
     
     ctx.fillText("PLAYER: " + playerScore ,10,70);
@@ -73,8 +78,36 @@ function pick( choice ){
 
 }
 
+window.onload = function(){
 
+    //set player stats
+    firebase.database().ref().push({
 
+        wins : 0,
+        losses : 0,
+        choice : 0,
+        score : 0,
+        curMsg: ""
+
+    }).then((snap) => {
+        myId = snap.key;
+        
+    });
+
+    firebase.database().ref().orderByKey().once( "value" )
+        .then(function(snap){
+
+            snap.forEach(childSnap => {
+                console.log(childSnap.key);
+            });
+        });
+
+    //remove self from database on window or tab exit or refresh
+    firebase.database().ref( ).onDisconnect().remove( );
+
+    
     
 
 
+
+}
